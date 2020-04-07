39d4f4f839b92a252220866f4c192822282e0fe7
SukkoPera <software@sukkology.net>
2020-04-07 20:30:24 +0200

    Update copyright notice

--- examples/MegaDriveMultiplexer/MegaDriveMultiplexer.ino
+++ examples/MegaDriveMultiplexer/MegaDriveMultiplexer.ino
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * This file is part of N64Pad for Arduino.                                    *
  *                                                                             *
- * Copyright (C) 2015 by SukkoPera                                             *
+ * Copyright (C) 2015-2020 by SukkoPera                                        *
  *                                                                             *
  * N64Pad is free software: you can redistribute it and/or modify              *
  * it under the terms of the GNU General Public License as published by        *
