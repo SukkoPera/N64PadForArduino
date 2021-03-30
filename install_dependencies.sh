3ed9493066c3457e03f7c345b54f477884628d1d
Giorgioggì <software@sukkology.net>
2021-03-30 17:58:47 +0200

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-wget https://github.com/MHeironimus/ArduinoJoystickLibrary/archive/refs/tags/v2.0.7.tar.gz -o ArduinoJoystickLibrary-2.0.7.tar.gz
+# This file is only used by the Arduino_CI GitHub action to install
+# the libraries some examples depend upon before compiling
+
+#wget https://github.com/MHeironimus/ArduinoJoystickLibrary/archive/refs/tags/v2.0.7.tar.gz -o ArduinoJoystickLibrary-2.0.7.tar.gz
+curl -o ArduinoJoystickLibrary-2.0.7.tar.gz https://github.com/MHeironimus/ArduinoJoystickLibrary/archive/refs/tags/v2.0.7.tar.gz
 tar zxvf ArduinoJoystickLibrary-2.0.7.tar.gz
 rm -f ArduinoJoystickLibrary-2.0.7.tar.gz
