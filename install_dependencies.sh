d6bc0e2c3b2da492e1dca76f0be3eae703e71c06
Giorgioggì <software@sukkology.net>
2021-03-30 17:51:51 +0200

    Create install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://github.com/MHeironimus/ArduinoJoystickLibrary/archive/refs/tags/v2.0.7.tar.gz -o ArduinoJoystickLibrary-2.0.7.tar.gz
+tar zxvf ArduinoJoystickLibrary-2.0.7.tar.gz
+rm -f ArduinoJoystickLibrary-2.0.7.tar.gz
